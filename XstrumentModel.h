74cb8b7e6b003a8c7d136e6270179116b9f68789
rfielding <rob.fielding@gmail.com>
2009-05-29 10:23:51 -0400

    Initial copyright.  Need to really have it right before this begins to resemble the actual Samchillian

--- XstrumentModel.h
+++ XstrumentModel.h
@@ -2,8 +2,17 @@
 //  XstrumentModel.h
 //  Xstrument
 //
-//  Created by Robert Fielding on 5/29/09.
-//  Copyright 2009 Check Point Software. All rights reserved.
+/*
+ The Samchillian is patented by Leon Gruenbaum. 
+ I have gotten permission to make this application available for general consumption. 
+ Refer to the original Samchillian 
+ http://www.samchillian.com 
+ if you have any questions about whether you need to be concerned about copyright
+ or patent issues (such as if you have implemented something similar yourself). 
+ Like the official Samchillian software, this is only for non commercial use, 
+ which means that you have no right to sell this or modified copies of this 
+ software without explicit permission.  
+ */
 //
 
 #import <Cocoa/Cocoa.h>
