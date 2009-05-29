c4d0e27c054f8ba5cdefa4ac0a479c45b1aa14d0
Robert Fielding <rfielding@mbp2.local>
2009-05-28 22:07:05 -0400

    Initial project state

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Xstrument
+//
+//  Created by Robert Fielding on 5/28/09.
+//  Copyright Check Point Software 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
